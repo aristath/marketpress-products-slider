08933bc86420948d30241ca8e592ccefbafe9547
Aristeides Stathopoulos <aristath@gmail.com>
2012-09-19 07:23:15 +0200

    improved. :)

--- marketpress-products-slider.php
+++ marketpress-products-slider.php
@@ -18,7 +18,7 @@ define('MPS_FEATURED_SLIDER_WIDTH',        1170);            //you should enter 
 define('MPS_FEATURED_SLIDER_HEIGHT',       300);             //the height of the featured slider (px)
 define('MPS_FEATURED_SLIDER_IMAGE_WIDTH',  580);             //the width of your slider image (px)
 define('MPS_FEATURED_SLIDER_IMAGE_TITLE',  'View Product');  //the title of your slider image. WIll be visible when hovering the image
-define('MPS_FEATURED_SLIDER_COUNT',        '3');             //how many products you want shown in the slider
+define('MPS_FEATURED_SLIDER_COUNT',        '1');             //how many products you want shown in the slider
 define('MPS_FEATURED_SLIDER_SPEED',        8000);            //time for slides (ms)
 define('MPS_SLIDER_TYPE',                  'slide');         //Select your animation type, 'fade' or 'slide'
 
@@ -65,17 +65,34 @@ function mps_featured_slider(){
 		jQuery('.home-slider .flexslider').flexslider({
 			slideshowSpeed: <?php echo MPS_FEATURED_SLIDER_SPEED ?>,
 			controlNav: false,
-			animation: <?php echo MPS_SLIDER_TYPE ?>
+			animation: '<?php echo MPS_SLIDER_TYPE ?>'
 		});
 	});
 	</script>
 	<style>
-	.home-slider .flexslider ul.slides li .flex-caption {
-		width: <?php echo (MPS_FEATURED_SLIDER_WIDTH - MPS_FEATURED_SLIDER_IMAGE_WIDTH); ?>px;
-	}
-	.home-slider .flexslider ul.slides li .image-wrapper {
-		width: <?php echo MPS_FEATURED_SLIDER_IMAGE_WIDTH ?>px;
-	}
+	.home-slider .flexslider ul.slides li .flex-caption {width: <?php echo (MPS_FEATURED_SLIDER_WIDTH - MPS_FEATURED_SLIDER_IMAGE_WIDTH); ?>px;}
+	.home-slider .flexslider ul.slides li .image-wrapper {width: <?php echo MPS_FEATURED_SLIDER_IMAGE_WIDTH ?>px;}
+	.home-slider {background-color:#ffffff;margin:0 -20px;}
+	.home-slider .flexslider {overflow: hidden;border:0;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;box-shadow:0 0 0;-webkit-box-shadow:0 0 0;-moz-box-shadow:0 0 0;-o-box-shadow:0 0 0;zoom:1;margin-bottom:10px;}
+	.home-slider .flexslider ul.slides { height:300px; }
+	.home-slider .flexslider ul.slides li {position:relative;height:253px;}
+	.home-slider .flexslider ul.slides li .flex-caption {padding:0;position:absolute;left:0;top:0;padding:40px 5px 10px 45px;background:#ffffff !important;color:#313131;font-size:14px;line-height:18px;height:100%;text-align:center;}
+	.home-slider .flexslider ul.slides li .flex-caption:after, .home-slider .flexslider ul.slides li .flex-caption:before {left:100%;border:solid transparent;content:" ";height:0;width:0;position:absolute;pointer-events:none;}
+	.home-slider .flexslider ul.slides li .flex-caption:after {border-left-color:#ffffff;border-width:20px;top:30%;margin-top:-20px;}
+	.home-slider .flexslider ul.slides li .flex-caption:before {border-left-color:#ffffff;border-width:26px;top:30%;margin-top:-26px;}
+	.home-slider .flexslider ul.slides li h2 {font-size:30px;line-height:35px;margin-bottom:10px;font-weight:bold;padding-right:25px;}
+	.home-slider .flexslider ul.slides li .description {font-size:13px;color:#525351;line-height:17px;padding-right:25px;}
+	.home-slider .flexslider ul.slides li .slider_product_meta { text-align:center; }
+	.home-slider .flexslider ul.slides li .slider_product_meta .mp_product_price {margin-bottom:30px;padding-right:25px;font-size:40px;line-height:44px;font-weight:bold;display:inline-block;}
+	.home-slider .flexslider ul.slides li .slider_product_meta .mp_product_price .mp_old_price {color:#bababa;padding-right:5px;font-weight:bold;text-decoration:line-through;}
+	.home-slider .flexslider ul.slides li .slider_product_meta .mp_buy_form { text-align:center; }
+	.home-slider .flexslider ul.slides li .slider_product_meta .mp_buy_form .mp_quantity { display:none; }
+	.home-slider .flexslider ul.slides li .image-wrapper {float:right;height:300px;}
+	.home-slider ul.flex-direction-nav {position:absolute;height:50px;width:120px;bottom:10px;right:10px;}
+	.home-slider ul.flex-direction-nav li a {opacity:0.8;background:#333;-webkit-border-radius:4px;-moz-border-radius:4px;border-radius:4px;width:45px;height:45px;}
+	.home-slider ul.flex-direction-nav li a:hover { opacity:1; }
+	.home-slider ul.flex-direction-nav li a.flex-prev {right:5px;background:#333 url("images/arrow_slider_left.png") no-repeat center center;left:20px;}
+	.home-slider ul.flex-direction-nav li a.flex-next {left:5px;background:#333 url("images/arrow_slider_right.png") no-repeat center center;left:75px;}
 	</style>
 	<div class="region-divider"></div>
 	<?php
@@ -261,9 +278,6 @@ function mps_product_photo( $echo = true, $post_id = NULL, $link = true, $width 
 function mps_scripts_and_styles() {
 	wp_register_style('flexslider-css', plugins_url('includes/flexslider/flexslider.css', __FILE__), false, null);
 	wp_enqueue_style('flexslider-css');
-	
-	wp_register_style('mps-custom-css', plugins_url('includes/style.css', __FILE__), false, null);
-	wp_enqueue_style('mps-custom-css');
 	
 	wp_register_script('flexslider-js', plugins_url('includes/flexslider/jquery.flexslider.min.js', __FILE__), false, null, false);
 	wp_enqueue_script('flexslider-js');
